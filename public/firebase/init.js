04fde25fb647082c974c315fc64869883c5fc3b5
anrisovich <34153344+anrisovich@users.noreply.github.com>
2022-01-06 03:43:01 +0300

    Update init.js

--- public/firebase/init.js
+++ public/firebase/init.js
@@ -1,7 +1,7 @@
 // <script src="/firebase/init.js" type="module"></script>
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+import { initializeApp } from "";
+import { getAnalytics } from "";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
